6439557f273d27f31590fd99ce222e0be67611e9
GrzegorzZiemba <gzgrzegorzziemba@gmail.com>
2021-02-09 12:44:36 +0100

    Adding comments

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import SearchInput from "./SearchInput";
 const OMDB_API_KEY = "46c460bb";
 
+// I choose t instead of s, becouse of that plot twist possibility /shwo|hide/
+// I Know that searching via 's' would give me an array of Searches and i can show them via button , iterate throught them using map or let them roll,
+
 const Search = () => {
 	const [data, setData] = useState("");
 	const [inputValue, setinputValue] = useState("");
 	//make titleOnSubmit variable to re-render only when submit is clicked
 	const [titleOnSubmit, setTitleOnSubmit] = useState("");
 
-	//make handlerFor plotTwist
+	//make handlerFor plotTwist to see if user Want's to see it or not
 	const [showPlotTwist, setPlotTwist] = useState(true);
 
 	// input callback handler to handle changes on the input area
@@ -39,10 +42,13 @@ const Search = () => {
 	useEffect(() => {
 		dataFrom();
 		console.log(data);
+		// if (poster) {
+		// 	getPoster();
+		// }
 	}, [titleOnSubmit, showPlotTwist]);
 
 	return (
-		<h1>
+		<div className="mainContainer">
 			<form
 				onSubmit={(e) => {
 					e.preventDefault();
@@ -56,11 +62,18 @@ const Search = () => {
 			<h1>{data.Title}</h1>
 			<h2>{data.Year}</h2>
 			<p>{data.Rated}</p>
-			<button onClick={() => setPlotTwist(!showPlotTwist)}>
-				Wanna see plot twist ?
-			</button>
+			{data.Title ? (
+				<>
+					<button onClick={() => setPlotTwist(!showPlotTwist)}>
+						Wanna see plot twist ?
+					</button>
+				</>
+			) : (
+				""
+			)}
+
 			<p>{data.Plot}</p>
-		</h1>
+		</div>
 	);
 };
 
